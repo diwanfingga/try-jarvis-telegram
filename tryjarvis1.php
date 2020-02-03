831a493d8cf1299dd60fcb9f14306e784ee60be1
Diwan Fingga <39618783+diwanfingga@users.noreply.github.com>
2020-02-03 12:47:54 +0700

    Update tryjarvis1.php

--- tryjarvis1.php
+++ tryjarvis1.php
@@ -1,19 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>TryJarvis</title>
+</head>
+<body>
+	<h1>TryJarvis is working!</h1>
 
-$botToken = "943426745:AAHOnwra1uIj57NTQ2yjcQvN2Xh1JSCvTsc";
-$website = "https://api.telegram.org/bot".$botToken;
+	<?php
 
-
-$update = file_get_contents($website."/getupdates");
-
-$updateArray = json_decode($update, TRUE);
-
-print_r($updateArray);
-
-$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-
-file_get_contents($website."/sendmessage?chat_id=".$chatID."$text=test");
+	$botToken = "943426745:AAHOnwra1uIj57NTQ2yjcQvN2Xh1JSCvTsc";
+	$website = "https://api.telegram.org/bot".$botToken;
 
 
+	$update = file_get_contents($website."/getupdates");
+//$update = file_get_contents("php://input");
 
-?>
+
+//print_r($update);
+
+	$updateArray = json_decode($update, TRUE);
+
+//	print_r($updateArray);
+
+	$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+//print_r($chatId);
+//	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+	$a = -1001384560859;
+	file_get_contents($website."/sendmessage?chat_id=".$a."&text=test");
+
+
+
+	?>
+	<form action="<?php echo $website.'/sendMessage'?>" method="get" enctype="multipart/form-data">
+
+		<input type="text" name="chat_id" value="<?php echo htmlspecialchars($a); ?>"/>
+		<input type="text" name="text"/>
+		<input type="submit" value="send"/>
+	</form>
+
+</body>
+</html>
+
+
